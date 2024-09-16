f82721c9d42b0badab2024f32ccb8ab44c33e17e
piercefriden <piercefri@outlook.com>
2024-09-16 14:14:31 -0700

    yas

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-<<<<<<< HEAD
+
 	$("#chillguyblack").click(function() {\
 		$("h2").hide();
 	});
-=======
   $("#chillguyblack").click(function() {
-	
+
   });
->>>>>>> b61e440d43af60808ace4a84a8e828f584f808b1
+
 });
